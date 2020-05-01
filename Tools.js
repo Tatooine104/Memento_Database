5dbdcb80439c387a254098be86b61a2846e9e619
Dmitry Shevchenko <primalevil@gmail.com>
2020-05-01 17:08:33 +0300

    Update Tools.js

--- Tools.js
+++ Tools.js
@@ -3,19 +3,12 @@ var primalevil = {
   test a contact
     @param {string} name - The contact name.
     @param {Arguments} options - Extra options
-    You can specify extra contact options:
-      {string} phone - The contact phone number.
-      {string} email - The contact email address.
-      {string} postal - The contact postal address.
-      {string} company - The contact company.
-      {string} jobTitle - The contact job title.
-      {string} notes - The contact notes.
       
     @example
     primalevil.test();
     */
     test: function() {
-	    var temp = "!!!!";
-	    return temp;
+	var temp = "!!!!";
+	return temp;
     }
 }
